8658698a92da049e6d42faeaf94233dad5dd53c2
xiaodoli <xiaodoli@microsoft.com>
2018-06-25 11:37:21 -0700

    ".gitignore renamed to .npmignore"

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,57 +4,55 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
-    prompting() {
-        // Have Yeoman greet the user.
-        this.log(
-            yosay(`Welcome to the ${chalk.red('generator-hello-world')} generator!`)
-        );
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(yosay(`Welcome to the ${chalk.red('generator-hello-world')} generator!`));
 
-        const prompts = [
-            {
-                type: 'input',
-                name: 'name',
-                message: 'Your project name',
-                default: this.appname
-            }
-        ];
+    const prompts = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: this.appname
+      }
+    ];
 
-        return this.prompt(prompts).then(props => {
-            // To access props later use this.props.someAnswer;
-            this.props = props;
-        });
-    }
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
 
-    writing() {
-        const tempalteFiles = [
-            '.editorconfig',
-            '.gitignore',
-            'tsconfig.json',
-            'tslint.json',
-            'webpack.config.js'
-        ];
+  writing() {
+    const tempalteFiles = [
+      '.editorconfig',
+      'tsconfig.json',
+      'tslint.json',
+      'webpack.config.js'
+    ];
 
-        this.fs.copyTpl(
-            this.templatePath('_package.json'),
-            this.destinationPath('package.json'),
-            {
-                name: this.props.name
-            }
-        );
-        this.fs.copyTpl(
-            this.templatePath('_bower.json'),
-            this.destinationPath('bower.json'),
-            {
-                name: this.props.name
-            }
-        );
+    this.fs.copyTpl(
+      this.templatePath('_package.json'),
+      this.destinationPath('package.json'),
+      {
+        name: this.props.name
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath('_bower.json'),
+      this.destinationPath('bower.json'),
+      {
+        name: this.props.name
+      }
+    );
+    this.fs.copy(this.templatePath('_gitignore'), this.destinationPath('.gitignore'));
 
-        tempalteFiles.forEach(element => {
-            this.fs.copy(this.templatePath(element), this.destinationPath(element));
-        });
-    }
+    tempalteFiles.forEach(element => {
+      this.fs.copy(this.templatePath(element), this.destinationPath(element));
+    });
+  }
 
-    install() {
-        this.installDependencies();
-    }
+  install() {
+    this.installDependencies();
+  }
 };
